492618f32f0afb84b2148157f7e743606c126eea
Feodor Kachinsky <fedor15zil@gmail.com>
2023-06-30 22:10:12 +0300

    Added spring data song file repository

---
+++ file-api/src/main/java/com/innowise/fileapi/repository/SongFileRepository.java
@@ -0,0 +1,7 @@
+package com.innowise.fileapi.repository;
+
+import com.innowise.fileapi.model.SongFile;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface SongFileRepository extends ListCrudRepository<SongFile,Integer> {
+}
