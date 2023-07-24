442f3cd18d224d5416a5810511d2f26260512ea5
Feodor Kachinsky <fedor15zil@gmail.com>
2023-07-25 00:21:10 +0300

    Fixed spring cloud config clients

--- api-gateway/src/test/java/com/innowise/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/innowise/apigateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.innowise.contractapi;
+package com.innowise.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ContractApiApplicationTests {
+class ApiGatewayApplicationTests {
 
     @Test
     void contextLoads() {
