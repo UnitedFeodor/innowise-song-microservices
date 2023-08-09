d0ea2628c1a514b363af28baeb6b539c36d22c4b
Feodor Kachinsky <fedor15zil@gmail.com>
2023-08-09 12:13:27 +0200

    Added basic routing

--- song-frontend/src/App.js
+++ song-frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Error404 from './components/Error404/Error404';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Upload from './components/Upload/Upload';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<Error404 />}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/upload" element={<Upload/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
