28741a92d926d3431db51c3f766fb60442c78807
Feodor Kachinsky <fedor15zil@gmail.com>
2023-08-14 10:27:20 +0200

    Added docker

--- song-frontend/src/config/environment.js
+++ song-frontend/src/config/environment.js
@@ -1,7 +1,7 @@
 export const environment = {
     baseUrl: 'http://localhost:8082/song-api/songs',
     issuerUrl: 'http://localhost:9000',
-    redirectUri: 'http://localhost:3000/login',
+    redirectUri: window.location.origin + '/login',
     clientId: 'song-enricher-client',
     clientSecret: 'secret'
 };
