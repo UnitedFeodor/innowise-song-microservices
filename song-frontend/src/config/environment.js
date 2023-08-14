5b395b80a1dbc9ed266200fdb0f9111f38432155
Feodor Kachinsky <fedor15zil@gmail.com>
2023-08-14 10:32:43 +0200

    Changed redirect uri

--- song-frontend/src/config/environment.js
+++ song-frontend/src/config/environment.js
@@ -1,7 +1,7 @@
 export const environment = {
     baseUrl: 'http://localhost:8082/song-api/songs',
     issuerUrl: 'http://localhost:9000',
-    redirectUri: window.location.origin + '/login',
+    redirectUri: window.location.origin + '/authorized',
     clientId: 'song-enricher-client',
     clientSecret: 'secret'
 };
